1d4bdd5237bb83a6809f78a855d40558d73cc73f
Dennishi0925 <36941125+Dennishi0925@users.noreply.github.com>
2021-07-13 17:17:27 +0800

    Add files via upload

---
+++ 2021-07-15_data-viz-practice.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+### Task(01): Visualize "US Streaming Market Share" 
+
+df_streaming <- tibble::tribble(
+  ~service, ~`2020`, ~`2021`,
+  "netflix",    29, 20,
+  "prime",      21, 16,
+  "hulu",       16, 13,
+  "disney",     12, 11,
+  "apple",       4,  5,
+  "peacock",     0,  5,
+  "hbo",         3, 12,
+  "paramount",   2,  3,
+  "other",      13, 15,
+)
+
+df_streaming
+
+### Task(02): Visualize "NTU Student Congress Members Election & Attendance Data" 
+df_candidate_final <- 
+  read_csv("https://raw.githubusercontent.com/Dennishi0925/RUserGroup_sharing/master/Sharing/data/NTUSC/df_candidate_final.csv") %>%
+  filter(term != "103-2")
+df_college_join <- read_csv("https://raw.githubusercontent.com/Dennishi0925/RUserGroup_sharing/master/Sharing/data/NTUSC/df_college_join.csv") %>%
+  filter(term != "103-2")
+df_sc_attnd <- read_csv("https://raw.githubusercontent.com/Dennishi0925/RUserGroup_sharing/master/Sharing/data/NTUSC/df_sc_attnd.csv") %>%
+  filter(term != "103-2")
+df_sc_attnd_vis <- read_csv("https://raw.githubusercontent.com/Dennishi0925/RUserGroup_sharing/master/Sharing/data/NTUSC/df_sc_attnd_vis.csv") %>%
+  filter(term != "103-2")
+df_vote_college <- read_csv("https://raw.githubusercontent.com/Dennishi0925/RUserGroup_sharing/master/Sharing/data/NTUSC/df_vote_college.csv") %>%
+  filter(start != "103-2")
