99e72ca6aad88de108cef5653909e2e410152263
Dennishi0925 <36941125+Dennishi0925@users.noreply.github.com>
2021-08-17 15:23:31 +0800

    meet03

---
+++ 2021-08-26_data-viz-practice.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+### Task: Visualize "Asia 2020 Netflix Ranking Data" 
+df_ranking_taiwan <- read_csv("2021-08-26_meeting03/df_ranking_taiwan.csv")
+df_ranking_japan <- read_csv("2021-08-26_meeting03/df_ranking_japan.csv")
+df_ranking_korea <- read_csv("2021-08-26_meeting03/df_ranking_korea.csv")
+df_ranking_hongkong <- read_csv("2021-08-26_meeting03/df_ranking_hongkong.csv")
+
+# (1) 台灣觀眾喜歡看什麼地方出產的影集/電影？
+
+# (2) 台灣哪部影集/電影最紅？
+
+# (3) Netflix 熱門程度與 IMDB 分數的關係
+
+# (4) 跨地比較：題目自訂
+
